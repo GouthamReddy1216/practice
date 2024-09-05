8ea1bf33e711716f5de79d443384d61f4d1fefd4
Goutham Reddy Somu <94057604+GouthamReddy1216@users.noreply.github.com>
2024-09-05 21:56:12 +0530

    corrected env.PORT to env.DB_PORT

--- server/db.js
+++ server/db.js
@@ -8,7 +8,7 @@ function createConnection() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     enableKeepAlive: true, // Keep the connection alive
     keepAliveInitialDelay: 10000, // Set an initial delay for keep-alive
   });
